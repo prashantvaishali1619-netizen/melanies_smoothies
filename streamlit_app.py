ec259dec4ce466613099092470468ed23e296347
prashantvaishali1619-netizen <prashantvaishali1619@gmail.com>
2026-02-23 00:30:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,18 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
 import requests
-ip = requests.get("https://ifconfig.me").text
-st.write("App Public IP:", ip)
+
+
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
   """Choose the fruits you want in you custom smoothie!  """
 )
+
+ip = requests.get("https://ifconfig.me").text
+st.write("App Public IP:", ip)
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be", name_on_order)
